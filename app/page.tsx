5e3f073fd9b08bb6c65f9f7e1ca3f73ea5a058e0
vcopeland <vcopeland@royallepage.ca>
2025-06-23 10:26:27 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import RoyalLePagePostTool from "@/components/RoyalLePagePostTool";
 
 export default function Home() {
   return (
-    <main className="p-8 max-w-4xl mx-auto">
+    <main>
       <RoyalLePagePostTool />
     </main>
   );
